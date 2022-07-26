826bccc3a6ae837c976c6ca8245202480c01cf4b
hanyi SEO <hanyiseo12@gmail.com>
2022-07-26 21:32:54 +0900

    Adding message

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -2,6 +2,6 @@
 public class HellowWorld {
 public static void main(String [] args) {
 	System.out.println("이거 언제 공부하냐,,,,");
-	System.out.println("ABCDEF"+"asd");
+	System.out.println("이번 8월 9월 동안 빡세게 자바공부하기!");
 }
 }
